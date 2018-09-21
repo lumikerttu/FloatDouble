8f5f050e78ef2de424d273eca016bae9463bab40
Kerttu Lumi <kerttu.lumi@gmail.com>
2018-09-21 12:13:34 +0300

    Initial commit, float and double converting Kilograms

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5d / 3d;
+
+        System.out.print("myIntValue = " + myIntValue);
+        System.out.print("myFloatValue = " + myFloatValue);
+        System.out.print("myDoubleValue = " + myDoubleValue);
+
+        double numPounds = 200d;
+        double convertedKilograms = numPounds * 0.45359237d;
+        System.out.println("Kilograms = " + convertedKilograms);
+    }
+}
